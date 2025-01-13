4f9573db6e2a53ffcdbf8fad691b9007388dd16b
MarcoDava <marco.a.dava@gmail.com>
2025-01-12 21:47:01 -0500

    creating the basic html and css

---
+++ PomodoroTimer.js
@@ -0,0 +1,11 @@
+const startEl = document.getElementById("start");
+const restartEl = document.getElementById("restart");
+const stopEl = document.getElementById("stop");
+const timerEl=document.getElementById("timer");
+
+
+
+startEl.addEventListener("click",() =>{
+    console.log("start");
+});
+
