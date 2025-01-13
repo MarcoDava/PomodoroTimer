f95eea4cac1e3580a1e3dd8694f9c06977bd2f13
MarcoDava <marco.a.dava@gmail.com>
2025-01-12 22:02:18 -0500

    finished the functionality of timer with js

--- PomodoroTimer.js
+++ PomodoroTimer.js
@@ -3,9 +3,35 @@ const restartEl = document.getElementById("restart");
 const stopEl = document.getElementById("stop");
 const timerEl=document.getElementById("timer");
 
+let interval;
+let timerLeft=1500;//25 minutes
 
+function updateTimer(){
+    let minutes=Math.floor(timerLeft / 60);
+    let seconds=timerLeft % 60;
+    let formattedTime='${minutes.toString().padStart(2,"0")} ":" ${seconds.toString().padStart(2,"0")';
+
+
+    timerEl.innerHTML = formattedTime;
+}
 
 startEl.addEventListener("click",() =>{
-    console.log("start");
+    interval = setInterval(()=>{
+        timerLeft--;
+        updateTimer();
+        if(timeLeft===0){
+            clearInterval(interval);
+            alert("Times Up!");
+            timerLeft=1500;
+        }
+    },1000);
+});
+restartEl.addEventListener("click",() =>{
+    clearInterval(interval);
+});
+stopEl.addEventListener("click",() =>{
+    clearInterval(interval);
+    timeLeft=1500;
+    updateTimer();
 });
 
